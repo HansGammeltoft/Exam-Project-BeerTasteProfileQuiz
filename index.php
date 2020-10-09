70a3b7a8b6fefbd797803891d2540bda7eb9fe64
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-10-09 23:05:48 +0200

    Added comments
    Added comments

--- index.php
+++ index.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="" content="">
     <title>Ølkassen.dk</title>
+    <! –– awesome fonts ––> 
     <script src="https://kit.fontawesome.com/9068d98231.js" crossorigin="anonymous"></script>    
     <link href="./css/stylesheet.css" rel="stylesheet">
 </head>
     <body>
         <header>
+            <! –– pulling all data from header.php ––> 
             <?php include('includes/header.php'); ?>
         </header>
         <main>
         </main>
         <footer>
+            <! –– pulling all data from footer.php ––> 
             <?php include('includes/footer.php'); ?>
         </footer>    
     </body>
