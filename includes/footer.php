e1db210447ba5ed63b399fd2720fa4243fc3e086
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-10-09 22:34:28 +0200

    Header + Footer
    Header + Footer coded with php and scss.

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<ul>
+    <li class="list-heading">INFORMATION</li>
+    <li class="information">Handelsbetingelser</li>    
+    <li class="information">Persondatapolitik</li>
+    <li class="information">Fødevarestyrelsens smiley</li>
+    <li class="information">Kontakt</li>
+    <li class="information">FAQ</li>    
+</ul>
+<ul>
+    <li class="list-heading">FACEBOOK</li>
+    <li><iframe src="https://www.facebook.com/v2.7/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df3047bfc88c8f58%26domain%3Doelkassen.dk%26origin%3Dhttps%253A%252F%252Foelkassen.dk%252Ff2eb015a79f82d8%26relation%3Dparent.parent&amp;container_width=329&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Foelkassen.dk%2F&amp;locale=da_DK&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;small_header=false&amp;width=500" style="border: none; visibility: visible; width: 329px; height: 181px;" class=""></iframe></li>    
+</ul>
+<ul>
+    <li class="list-heading">INSTAGRAM</li>
+    <li id="list-blue">No images available at the moment</li>    
+    <li id="list-button">Følg Ølkassen.dk</li>   
+</ul>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="" content="">
+    <title>Ølkassen.dk</title>
+    <script src="https://kit.fontawesome.com/9068d98231.js" crossorigin="anonymous"></script>    
+    <link href="./css/stylesheet.css" rel="stylesheet">
+</head>
+    <body>
+        <header>
+            <?php include('includes/header.php'); ?>
+        </header>
+        <main>
+        </main>
+        <footer>
+            <?php include('includes/footer.php'); ?>
+        </footer>    
+    </body>
+</html>
