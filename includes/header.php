7c45af2e4158058d40daff53ba9fa1ef6f9b30c5
Hans Peter Gammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-11-10 09:54:22 +0100

    Header and main is responsive
    Stille some work needs to be done on header.

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,35 @@
 <nav>
     <div id="upper-nav">
-        <div id="img-text">
-            <img src="./images/oelkassen-logo.png" alt="Et billede af ølkassens logo">
-            <div class="logo-text">
-                <p>Fri fragt ved køb for 600 kr.</p>
-                <p>Pickup i KBH, Kgs. Lyngby, Lejre</p>        
+        <div id="upper-nav-wrapper">
+            <div id="img-text">
+                <img src="./images/oelkassen-logo.png" alt="Et billede af ølkassens logo">
+                <div class="logo-text">
+                    <p>Fri fragt ved køb for 600 kr.</p>
+                    <p>Pickup i KBH, Kgs. Lyngby, Lejre</p>        
+                </div>
+                <input value="Søg i alle produkter">
+            </div>    
+            <div id="list-cart">
+                <ul id="nav-menu1">
+                    <li class="nav-menu-text">Forside</li>
+                    <li class="nav-menu-text">Sortiment</li>
+                    <li class="nav-menu-text">FAQ</li>
+                    <li class="nav-menu-text">Kontakt</li>
+                    <li class="nav-menu-text">Konto</li>
+                    <li><img src="./images/en.png" alt="Et billede af Union Jack"></li>      
+                </ul>
+                <ul id="nav-menu2">
+                    <li class="nav-menu-text"><i class="fas fa-bars"></i></li>
+                    <li><img src="./images/en.png" alt="Et billede af Union Jack"></li>      
+                </ul>            
+                <div id="cart">
+                    <i class="fas fa-shopping-cart"></i>
+                </div> 
             </div>
-            <input value="Søg i alle produkter">
-        </div>    
-        <div id="list-cart">
-            <ul id="nav-menu1">
-                <li class="nav-menu-text">Forside</li>
-                <li class="nav-menu-text">Sortiment</li>
-                <li class="nav-menu-text">FAQ</li>
-                <li class="nav-menu-text">Kontakt</li>
-                <li class="nav-menu-text">Konto</li>
-                <li><img src="./images/en.png" alt="Et billede af Union Jack"></li>      
-            </ul>
-            <ul id="nav-menu2">
-                <li class="nav-menu-text"><i class="fas fa-bars"></i></li>
-                <li><img src="./images/en.png" alt="Et billede af Union Jack"></li>      
-            </ul>            
-            <div id="cart">
-                <i class="fas fa-shopping-cart"></i>
-            </div> 
-        </div>    
+        </div>
+        <input id="phone-input" value="Søg i alle produkter">  
     </div>
+         
     <div id="lower-nav">
         <ul>
             <li>Sensommer IPA udsalg</li>
