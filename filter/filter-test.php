e0d20291fba406f469e692dc9bfd9d19680a4718
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-08 09:08:51 +0100

    Filter function works

--- filter/filter-test.php
+++ filter/filter-test.php
@@ -28,8 +28,8 @@
     ?>
       <li>
         <div class="">
-          <label>
-            <input type="radio" name="radio1" value="<?= $row['answer']; ?>" id="answer"><?= $row['answer'];  ?>
+          <label class="product_check">
+            <input type="radio" name="radio1" value="<?= $row['answer']; ?>" id="answer1"><?= $row['answer'];  ?>
           </label>
         </div>
       </li>
@@ -42,8 +42,8 @@
       ?>
         <li>
           <div class="">
-            <label>
-              <input type="radio" name="radio2" value="<?= $row['answer']; ?>" id="answer"><?= $row['answer'];  ?>
+            <label class="product_check">
+              <input type="radio" name="radio2" value="<?= $row['answer']; ?>" id="answer2"><?= $row['answer'];  ?>
             </label>
           </div>
         </li>
@@ -56,8 +56,8 @@
       ?>
         <li>
           <div class="">
-            <label>
-              <input type="radio" name="radio3" value="<?= $row['answer']; ?>" id="answer"><?= $row['answer'];  ?>
+            <label class="product_check">
+              <input type="radio" name="radio3" value="<?= $row['answer']; ?>" id="answer3"><?= $row['answer'];  ?>
             </label>
           </div>
         </li>
@@ -70,48 +70,83 @@
       ?>
         <li>
           <div class="">
-            <label>
-              <input type="radio" name="radio4" value="<?= $row['answer']; ?>" id="answer"><?= $row['answer'];  ?>
+            <label class="product_check">
+              <input type="radio" name="radio4" value="<?= $row['answer']; ?>" id="answer4"><?= $row['answer'];  ?>
             </label>
           </div>
         </li>
         <?php } ?>
       <hr>
     </ul>
+        <h5 class="text-center" id="textChange">All Products</h5>    
     <div class="row" id="result">
       <div>
-      <?php
-      //SELECTs from the data base, with the use of innter joins, to join the tables.
-      $sql="SELECT name, taste, answer FROM table_beers
-              INNER JOIN table_beer_taste ON table_beers.productID = table_beer_taste.productID
-              INNER JOIN table_taste ON table_beer_taste.tasteID = table_taste.tasteID
-              INNER JOIN table_answer_taste ON table_taste.tasteID = table_answer_taste.tasteID
-              INNER JOIN table_answers ON table_answer_taste.answerID = table_answers.answerID";
-        //It performs a query function against the sql variable, which selects information from the database
-        $result=$conn->query($sql);
-        //beername variable is SET to an empty string
-        $beername = '';
-        //while loop, it loops the result from the query function
-        while($row=$result->fetch_assoc())
-        {
-        //If $beername is EQUAL to empty, which it is, then SET it as the values in the "name" column in the database, and put it inside a list.
-        if ($beername == '') {
-          $beername = $row['name'];
-          echo '<ul><li>' . $row['name'] .' '. $row['answer'];
-        //If the "name" from the database ISN'T EQUAL to the previous $beername, then close the list, and echo out the next $beername.
-        }elseif ($row['name'] != $beername) {
-          $beername = $row['name'];
-          echo '</li><li>';
-          echo $row['name'] .' '. $row['answer'];
-        //Else only echo the values in the "answer" column.
-        }else {
-          echo $row['answer'];
-        }
-          ?>
-    <?php } ?>
-  </li>
-</ul>
-</div>
-</div>
+        <?php
+        //SELECTs from the data base, with the use of innter joins, to join the tables.
+        $sql="SELECT * FROM table_beers
+                INNER JOIN table_brewery ON table_beers.breweryID = table_brewery.breweryID
+				        INNER JOIN table_type ON table_beers.typeID = table_type.typeID
+				        INNER JOIN table_alcohol ON table_beers.alcoholID = table_alcohol.alcoholID
+				        INNER JOIN table_size ON table_beers.sizeID = table_size.sizeID
+				        INNER JOIN table_price ON table_beers.priceID = table_price.priceID
+                INNER JOIN table_beer_taste ON table_beers.productID = table_beer_taste.productID
+                INNER JOIN table_taste ON table_beer_taste.tasteID = table_taste.tasteID
+                INNER JOIN table_answer_taste ON table_taste.tasteID = table_answer_taste.tasteID
+                INNER JOIN table_answers ON table_answer_taste.answerID = table_answers.answerID";
+          //It performs a query function against the sql variable, which selects information from the database
+          $result=$conn->query($sql);
+          //beername variable is SET to an empty string
+          $beername = '';
+          //while loop, it loops the result from the query function
+          while($row=$result->fetch_assoc())
+          {
+            //If $beername is EQUAL to empty, which it is, then SET it as the values in the "name" column in the database, and put it inside a list.
+            if ($beername == '') {
+              $beername = $row['name'];
+              echo '<ul><li>' . $row['name'] .' '. $row['answer'];
+            //If the "name" from the database ISN'T EQUAL to the previous $beername, then close the list, and echo out the next $beername.
+            }elseif ($row['name'] != $beername) {
+              $beername = $row['name'];
+              echo '</li><li>';
+              echo $row['name'] .' '. $row['answer'];
+            //Else only echo the values in the "answer" column.
+            }else {
+              echo $row['answer'];
+            }
+          }
+        ?>
+        </li></ul>
+      </div>
+    </div>
+    <script type="text/javascript">
+        $(document).ready(function(){
+          $(".product_check").click(function(){
+
+            var action = 'data';
+            var answer1 = get_filter_text('answer1');
+            var answer2 = get_filter_text('answer2');
+            var answer3 = get_filter_text('answer3');
+            var answer4 = get_filter_text('answer4');
+
+            $.ajax({
+              url:'action.php',
+              method: 'POST',
+              data:{action:action,answer1:answer1,answer2:answer2,answer3:answer3,answer4:answer4},
+              success:function(response){
+                $("#result").html(response);
+                $("#textChange").text("Filtedered Products");
+              }
+            });
+          });
+
+          function get_filter_text(text_id){
+            var filterData = [];
+            $('#'+text_id+':checked').each(function(){
+              filterData.push($(this).val());
+            });
+            return filterData;
+          }
+        });
+    </script>
   </body>
 </html>
--- test.php
+++ test.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="" content="">
     <title>Ølkassen.dk</title>
-    <!–– awesome fonts ––> 
-    <script src="https://kit.fontawesome.com/9068d98231.js" crossorigin="anonymous"></script>    
+    <!–– awesome fonts ––>
+    <script src="https://kit.fontawesome.com/9068d98231.js" crossorigin="anonymous"></script>
     <link href="./css/stylesheet.css" rel="stylesheet">
 </head>
     <body>
         <header>
-            <!–– pulling all data from header.php ––> 
-            <?php include('includes/header.php'); ?>            
+            <!–– pulling all data from header.php ––>
+            <?php include('includes/header.php'); ?>
         </header>
         <main>
-            <!–– pulling all data from index_main.php ––> 
-            <?php include('includes/productpage.php'); ?>            
+            <!–– pulling all data from index_main.php ––>
+            <?php include('includes/question-type-6-answers.php'); ?>
         </main>
         <footer>
-            <!–– pulling all data from footer.php ––> 
+            <!–– pulling all data from footer.php ––>
             <?php include('includes/footer.php'); ?>
-        </footer>    
+        </footer>
     </body>
 </html>
