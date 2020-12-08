48a9156e42a35a21264788cded868be602045189
majalindved <60427034+majalindved@users.noreply.github.com>
2020-12-08 13:00:50 +0100

    Delete config.php
    Fil slettet. Database connection rettet til oprindelige fil "php-db-projekt7"

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connecting to the database
-  $conn = new mysqli ("localhost","root","","projekt7");
-  //if it doenst connect to database, then post "Connection failed!"
-  if($conn->connect_error) {
-    die("Connection Failed!".$conn->connect_error);
-  }
- ?>
