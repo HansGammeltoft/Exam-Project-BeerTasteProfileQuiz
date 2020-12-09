7a364660c36badfee8547f4a25f3b5278fce11f3
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-09 17:56:54 +0100

    Loading screen added as a test
    I am testing the loading screen, there are some minor issues with the framing.

---
+++ Unused files/animation/animation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css.css">
+  </head>
+  <body>
+    <div class="character">
+      <img class="character_spritesheet" src="animation.png" alt="Beer filling up">
+    </div>
+  </body>
+</html>
