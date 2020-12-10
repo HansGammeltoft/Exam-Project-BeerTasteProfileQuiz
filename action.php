f4c630cbffd1e41356c61c8cc0156ddca6e5a799
majalindved <60427034+majalindved@users.noreply.github.com>
2020-12-10 15:04:13 +0100

    Update action.php
    Ny HTML/CSS til resultatside

--- action.php
+++ action.php
@@ -37,69 +37,14 @@
         if ($beername == '') {
           $beername = $row['name'];
           echo '<div class="beername_wrapper"><div>';
-          echo '<img style="height:200px;" src="./images/oelkassenlogod.png">';
-          echo '<div class="description-left">
-                  <div class="description">
-                      <h2 class="beer-name">'; echo $row['name']; echo '</h2>';
-                      echo '<p class="beer-description">'; echo $row['description']."</br>".$row['taste'] .' '; echo '</p>';
-                  echo '</div>
-                  <div class="description-price-button">
-                      <div class="description-price">
-                          <p class="price">'; echo $row['price']; echo '</p>';
-                          echo '<p class="stock-status">På lager</p>';
-                      echo '</div>
-                      <div class="description-button">
-                          <button>LÆG I KURV</button>
-                      </div>
-                  </div>
-                </div>';
-          echo '<div class="description-right">
-                  <div class="description-list">
-                    <ul>
-                      <li><p class="description-list-bold">Kategori:</p><p class="description-list-normal">'; echo $row['categoryID']; echo '</p></li>
-                      <li><p class="description-list-bold">Type:</p><p class="description-list-normal">'; echo $row['type']; echo '</p></li>
-                      <li><p class="description-list-bold">Alkohol:</p><p class="description-list-normal">'; echo $row['alcohol']; echo '</p></li>
-                      <li><p class="description-list-bold">Størrelse:</p><p class="description-list-normal">'; echo $row['size']; echo '</p></li>
-                      <li><p class="description-list-bold">Bryggeri:</p><p class="description-list-normal">'; echo $row['brewery']; echo '</p></li>
-                      <li><p class="description-list-bold">Land:</p><p class="description-list-normal">'; echo $row['countryID']; echo '</p></li>
-                      <li><p class="description-list-bold">Rating:</p><p class="description-list-normal">'; echo $row['ratingID']; echo '</p></li>
-                    </ul>
-                  </div>
-                </div>';
-
+          echo '<img style="height:200px;" src="./images/oelkassenlogod.png"></br>';
+          echo '<h2>'; echo $row['name'] .' ';
         //If the "name" from the database ISN'T EQUAL to the previous $beername, then close the list, and echo out the next $beername.
         }elseif ($row['name'] != $beername) {
           $beername = $row['name'];
           echo '</div><div>';
-          echo '<img style="height:200px;" src="./images/oelkassenlogod.png">';
-          echo '<div class="description-left">
-                  <div class="description">
-                      <h2 class="beer-name">'; echo $row['name']; echo '</h2>';
-                      echo '<p class="beer-description">'; echo $row['description']."</br>".$row['taste'] .' '; echo '</p>';
-                  echo '</div>
-                  <div class="description-price-button">
-                      <div class="description-price">
-                          <p class="price">'; echo $row['price']; echo '</p>';
-                          echo '<p class="stock-status">På lager</p>';
-                      echo '</div>
-                      <div class="description-button">
-                          <button>LÆG I KURV</button>
-                      </div>
-                  </div>
-                </div>';
-          echo '<div class="description-right">
-                  <div class="description-list">
-                    <ul>
-                      <li><p class="description-list-bold">Kategori:</p><p class="description-list-normal">'; echo $row['categoryID']; echo '</p></li>
-                      <li><p class="description-list-bold">Type:</p><p class="description-list-normal">'; echo $row['type']; echo '</p></li>
-                      <li><p class="description-list-bold">Alkohol:</p><p class="description-list-normal">'; echo $row['alcohol']; echo '</p></li>
-                      <li><p class="description-list-bold">Størrelse:</p><p class="description-list-normal">'; echo $row['size']; echo '</p></li>
-                      <li><p class="description-list-bold">Bryggeri:</p><p class="description-list-normal">'; echo $row['brewery']; echo '</p></li>
-                      <li><p class="description-list-bold">Land:</p><p class="description-list-normal">'; echo $row['countryID']; echo '</p></li>
-                      <li><p class="description-list-bold">Rating:</p><p class="description-list-normal">'; echo $row['ratingID']; echo '</p></li>
-                    </ul>
-                  </div>
-                </div>';
+          echo '<img style="height:200px;" src="./images/oelkassenlogod.png"></br>';
+          echo '<h2>'; echo $row['name'] .' ';
         //Else only echo the values in the "answer" column.
         }else {
           echo $row['taste'] .' ';
@@ -118,4 +63,5 @@
            $beername_wrapper.children(':nth-of-type(n+6)').hide();
        }
    });
+
  </script>
