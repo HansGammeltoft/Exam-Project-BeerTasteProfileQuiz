c811e5b7f19b4de552140c8cc4978957c9bfdf00
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-10 09:47:27 +0100

    Update of loadingscreen

--- Unused files/animation/animation.php
+++ Unused files/animation/animation.php
@@ -6,16 +6,25 @@
     <link rel="stylesheet" href="css.css">
   </head>
   <body>
-    <div class="loader-wrapper">
-      <div class="loader-logo">
-        <img class="logouflasker" src="logouflasker.png" alt="">
-      </div>
-      <div class="loader-animation">
-        <img class="animation_spritesheet" src="animation.png" alt="Beer filling up">
-      </div>
-      <div class="loader-text">
-        <p>Lige et øjeblik...</p>
-        <p>Vi finder de perfekte øl til dig</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>
+    <p>forwardsforwardsforwardsforwardsforwardsforwards</p>                        
+    <div class="loader-popup">
+      <div class="loader-wrapper">
+        <div class="loader-logo">
+          <img class="logouflasker" src="logouflasker.png" alt="">
+        </div>
+        <div class="loader-animation">
+          <img class="animation_spritesheet" src="Animation1.png" alt="Beer filling up">
+        </div>
+        <div class="loader-text">
+          <p>Lige et øjeblik...</p>
+          <p>Vi finder de perfekte øl til dig</p>
+        </div>
       </div>
     </div>
   </body>
