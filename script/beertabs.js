2564fe438d4d8a84914e802b70e68206deb693f5
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-13 23:49:59 +0100

    Js

--- script/beertabs.js
+++ script/beertabs.js
@@ -1,15 +1,15 @@
-function openBeer(evt, beerName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(beerName).style.display = "flex";
-  evt.currentTarget.className += " active";
-}
+ function openBeer(evt, beerName) {
+   var i, tabcontent, tablinks;
+   tabcontent = document.getElementsByClassName("tabcontent");
+   for (i = 0; i < tabcontent.length; i++) {
+     tabcontent[i].style.display = "none";
+   }
+   tablinks = document.getElementsByClassName("tablinks");
+   for (i = 0; i < tablinks.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" active", "");
+   }
+   document.getElementById(beerName).style.display = "flex";
+   evt.currentTarget.className += " active";
+ }
 
-document.getElementById("defaultOpen").click();
+ document.getElementById("defaultOpen").click();
--- script/showhide.js
+++ script/showhide.js
@@ -1,25 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('');test-wrapper
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.lenght}
-  for (i = 0; i < slides.lenght; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
-
 //frem
 $(document).ready(function(){
   $(function() {
