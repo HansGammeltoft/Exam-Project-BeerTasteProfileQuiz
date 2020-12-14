7ce77ce8b718e091baa51499ca39dcc483e28147
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-14 09:17:44 +0100

    Script

--- script/beertabs.js
+++ script/beertabs.js
@@ -1,15 +1,33 @@
- function openBeer(evt, beerName) {
-   var i, tabcontent, tablinks;
-   tabcontent = document.getElementsByClassName("tabcontent");
-   for (i = 0; i < tabcontent.length; i++) {
-     tabcontent[i].style.display = "none";
+ if ( $(window).width() <= 716 ) {
+   function openBeer(evt, beerName) {
+     var i, tabcontent, tablinks;
+     tabcontent = document.getElementsByClassName("tabcontent");
+     for (i = 0; i < tabcontent.length; i++) {
+       tabcontent[i].style.display = "block";
+     }
+     tablinks = document.getElementsByClassName("tablinks");
+     for (i = 0; i < tablinks.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" active", "");
+     }
+     document.getElementById(beerName).style.display = "flex";
+     evt.currentTarget.className += " active";
    }
-   tablinks = document.getElementsByClassName("tablinks");
-   for (i = 0; i < tablinks.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" active", "");
-   }
-   document.getElementById(beerName).style.display = "flex";
-   evt.currentTarget.className += " active";
- }
 
- document.getElementById("defaultOpen").click();
+   document.getElementById("defaultOpen").click();
+  }else{
+    function openBeer(evt, beerName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(beerName).style.display = "flex";
+      evt.currentTarget.className += " active";
+    }
+
+    document.getElementById("defaultOpen").click();
+  }
