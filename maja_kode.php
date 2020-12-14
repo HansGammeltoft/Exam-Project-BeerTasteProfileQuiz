a4b2f8fdeab53bcb7d77182c7ffbde9903971430
majalindved <60427034+majalindved@users.noreply.github.com>
2020-12-14 10:24:05 +0100

    Update maja_kode.php
    Mere backup

--- maja_kode.php
+++ maja_kode.php
@@ -520,3 +520,123 @@ if ($beername == '') {
  //   }
  //   slides[slideIndex-1].style.display = "block";
  // }
+
+
+ //frem
+ $(document).ready(function(){
+   $(function() {
+      $('button#q2').click(function() {
+         $('div#q2_hidden').show();
+         return false;
+      });
+   });
+ });
+
+ $(document).ready(function(){
+   $(function() {
+      $('button#q2').click(function() {
+         $('div#q1_hidden').hide();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#q3').click(function() {
+         $('div#q3_hidden').show();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#q3').click(function() {
+         $('div#q2_hidden').hide();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#q4').click(function() {
+         $('div#q4_hidden').show();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#q4').click(function() {
+         $('div#q3_hidden').hide();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#showResult').click(function() {
+         $('div#q4_hidden').hide();
+         return false;
+      });
+   });
+ });
+   //Tilbage
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ1').click(function() {
+         $('div#q1_hidden').show();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ1').click(function() {
+         $('div#q2_hidden').hide();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ2').click(function() {
+         $('div#q2_hidden').show();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ2').click(function() {
+         $('div#q3_hidden').hide();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ3').click(function() {
+         $('div#q3_hidden').show();
+         return false;
+      });
+   });
+ });
+ $(document).ready(function(){
+   $(function() {
+      $('button#backQ3').click(function() {
+         $('div#q4_hidden').hide();
+         return false;
+      });
+   });
+ });
+   //show final result (limit 5)
+ $(document).ready(function(){
+   $(function() {
+      $('button#showResult').click(function() {
+         $('section.result-dd').show();
+         $('section.test').hide();
+         $('div#result').show();
+         return false;
+      });
+   });
+ });
