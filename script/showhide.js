e610be97429afe89da02a48e398e9e75b7af7251
majalindved <60427034+majalindved@users.noreply.github.com>
2020-12-14 10:24:49 +0100

    Update showhide.js
    Redundans slettet.
    Kode ændret til ny format.

--- script/showhide.js
+++ script/showhide.js
@@ -1,118 +1,33 @@
-//frem
-$(document).ready(function(){
-  $(function() {
-     $('button#q2').click(function() {
-        $('div#q2_hidden').show();
-        return false;
-     });
-  });
-});
+var spgIndex = 1;
+showSpg(spgIndex);
 
+function plusSpg(n) {
+  showSpg(spgIndex += n);
+}
+
+function currentSpg(n) {
+  showSpg(spgIndex = n);
+}
+
+function showSpg(n) {
+  var i;
+  var spg = document.getElementsByClassName('test-wrapper');
+  if (n > spg.length) {spgIndex = 1}
+  if (n < 1) {spgIndex = spg.length}
+  for (i = 0; i < spg.length; i++) {
+    spg[i].style.display = "none";
+  }
+  spg[spgIndex-1].style.display = "block";
+}
+
+//show final result (limit 5)
 $(document).ready(function(){
-  $(function() {
-     $('button#q2').click(function() {
-        $('div#q1_hidden').hide();
-        return false;
-     });
-  });
+$(function() {
+   $('button#showResult').click(function() {
+      $('section.result-dd').show();
+      $('section.test').hide();
+      $('div#result').show();
+      return false;
+   });
 });
-$(document).ready(function(){
-  $(function() {
-     $('button#q3').click(function() {
-        $('div#q3_hidden').show();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#q3').click(function() {
-        $('div#q2_hidden').hide();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#q4').click(function() {
-        $('div#q4_hidden').show();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#q4').click(function() {
-        $('div#q3_hidden').hide();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#showResult').click(function() {
-        $('div#q4_hidden').hide();
-        return false;
-     });
-  });
-});
-  //Tilbage
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ1').click(function() {
-        $('div#q1_hidden').show();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ1').click(function() {
-        $('div#q2_hidden').hide();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ2').click(function() {
-        $('div#q2_hidden').show();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ2').click(function() {
-        $('div#q3_hidden').hide();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ3').click(function() {
-        $('div#q3_hidden').show();
-        return false;
-     });
-  });
-});
-$(document).ready(function(){
-  $(function() {
-     $('button#backQ3').click(function() {
-        $('div#q4_hidden').hide();
-        return false;
-     });
-  });
-});
-  //show final result (limit 5)
-$(document).ready(function(){
-  $(function() {
-     $('button#showResult').click(function() {
-        $('section.result-dd').show();
-        $('section.test').hide();
-        $('div#result').show();
-        return false;
-     });
-  });
 });
