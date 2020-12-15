3d888e9377143fb5acae63beea37747c7aa177bc
HansGammeltoft <65598393+HansGammeltoft@users.noreply.github.com>
2020-12-16 00:41:51 +0100

    Meta viewport added

--- filter-test.php
+++ filter-test.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="da" dir="ltr">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <title></title>
     <link href="./css/stylesheet.css" rel="stylesheet">
